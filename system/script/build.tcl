dbb195f9fd8c127071e82bdea66d9e006e928f33
Martin Jerabek <jerabma7@fel.cvut.cz>
2016-04-01 10:44:59 +0200

    system: build fix, removed generated HDL wrappers

--- system/script/build.tcl
+++ system/script/build.tcl
@@ -3,6 +3,19 @@ set jobs 4
 open_project ../project/canbench.xpr
 reset_run synth_1
 reset_run impl_1
+
+set design_file ../src/top/top.bd
+set obj [get_files $design_file]
+generate_target all $obj
+export_ip_user_files -of_objects $obj -no_script -force -quiet
+
+#foreach ip [get_ips] {
+#	create_ip_run $ip
+#}
+#launch_run -jobs 4 {top_rst_processing_system7_0_100M_0_synth_1 top_processing_system7_0_1_synth_1 top_can_merge_0_1_synth_1}
+#launch_run -jobs 4 [get_ips]
+#export_simulation -of_objects $obj -directory ../project/canbench.ip_user_files/sim_scripts -force -quiet
+
 launch_runs synth_1 -jobs $jobs
 wait_on_run synth_1
 launch_runs impl_1 -jobs $jobs
