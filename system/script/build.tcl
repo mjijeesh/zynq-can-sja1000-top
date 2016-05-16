7f7e6aaabd616c418aab8facb96ef4738d14720e
Martin Jerabek <jerabma7@fel.cvut.cz>
2016-05-16 19:41:44 +0200

    system: updated build script for new Vivado version

--- system/script/build.tcl
+++ system/script/build.tcl
@@ -24,5 +24,5 @@ launch_runs impl_1 -jobs $jobs
 wait_on_run impl_1
 launch_runs impl_1 -jobs $jobs -to_step write_bitstream
 wait_on_run impl_1
-file copy -force ../project/canbench.runs/impl_1/top_wrapper.sysdef ../system.hdf
+file copy -force ../project/canbench.runs/impl_1/top_wrapper.hwdef ../system.hdf
 file copy -force ../project/canbench.runs/impl_1/top_wrapper.bit ../system.bit
